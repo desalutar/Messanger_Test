049ff08a500b63c7ca621b9510a250ce3c113671
desalutar <desalutare@gmail.com>
2023-12-14 23:43:47 +0300

    added cell in for views chat screen

---
+++ Messanger_Test/ControllerChats/Cell/ChatsCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChatsCell.swift
+//  Messanger_Test
+//
+//  Created by Ишхан Багратуни on 14.12.23.
+//
+
+import UIKit
+
+class ChatsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
